95f1ae9295331523bde20944b8d3f06294166713
shobhitsharma <ishobhitsharma@gmail.com>
2017-08-15 00:43:10 +0200

    Re-indent codebase

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ var xml2js = require('xml2js');
  * @constructor
  */
 function Parser(options) {
-	this.options = options || {};
-	return this;
+  this.options = options || {};
+  return this;
 }
 
 /**
@@ -34,15 +34,15 @@ function Parser(options) {
  * @param callback
  */
 Parser.prototype.execute = function execute(files, callback) {
-	assert.ok(files instanceof Array, "argument 'files' must be an array");
-	assert.equal(typeof callback, 'function', "argument 'callback' must be a function");
+  assert.ok(files instanceof Array, "argument 'files' must be an array");
+  assert.equal(typeof callback, 'function', "argument 'callback' must be a function");
 
-	async.map(files, this.parse, function (err, results) {
-		if (err) {
-			return callback(err);
-		}
-		this.bufferize(results, callback);
-	}.bind(this));
+  async.map(files, this.parse, function (err, results) {
+    if (err) {
+      return callback(err);
+    }
+    this.bufferize(results, callback);
+  }.bind(this));
 };
 
 /**
@@ -52,43 +52,43 @@ Parser.prototype.execute = function execute(files, callback) {
  * @param {Function} callback
  */
 Parser.prototype.parse = function parse(file, callback) {
-	assert.equal(typeof file, 'string', "argument 'file' must be a string");
-	assert.equal(typeof callback, 'function', "argument 'callback' must be a function");
+  assert.equal(typeof file, 'string', "argument 'file' must be a string");
+  assert.equal(typeof callback, 'function', "argument 'callback' must be a function");
 
-	fs.readFile(file, function (err, data) {
-		if (err) {
-			return callback(err);
-		}
+  fs.readFile(file, function (err, data) {
+    if (err) {
+      return callback(err);
+    }
 
-		var content = {};
-		JSZip().loadAsync(data)
-			.then(function (zip) {
-				async.each(Object.keys(zip.files), function (key, cb) {
-					var ext = key.substr(key.lastIndexOf('.'));
+    var content = {};
+    JSZip().loadAsync(data)
+      .then(function (zip) {
+        async.each(Object.keys(zip.files), function (key, cb) {
+          var ext = key.substr(key.lastIndexOf('.'));
 
-					if (ext === '.xml' || ext === '.rels') {
-						zip.file(key).async('string')
-							.then(function (xml) {
-								xml2js.parseString(xml, function (err, json) {
-									if (err) {
-										return cb(err);
-									}
-									content[key] = json;
-									cb(null);
-								});
-							});
-					} else {
-						content[key] = zip.file(key).async('string');
-						cb(null);
-					}
-				}, function (err) {
-					if (err) {
-						return callback(err);
-					}
-					callback(null, content);
-				});
-			});
-	});
+          if (ext === '.xml' || ext === '.rels') {
+            zip.file(key).async('string')
+              .then(function (xml) {
+                xml2js.parseString(xml, function (err, json) {
+                  if (err) {
+                    return cb(err);
+                  }
+                  content[key] = json;
+                  cb(null);
+                });
+              });
+          } else {
+            content[key] = zip.file(key).async('string');
+            cb(null);
+          }
+        }, function (err) {
+          if (err) {
+            return callback(err);
+          }
+          callback(null, content);
+        });
+      });
+  });
 };
 
 /**
@@ -98,50 +98,50 @@ Parser.prototype.parse = function parse(file, callback) {
  * @param callback
  */
 Parser.prototype.bufferize = function bufferize(xmls, options, callback) {
-	if (typeof options === 'function') {
-		callback = options;
-		options = {};
-	}
-	options = options || {};
+  if (typeof options === 'function') {
+    callback = options;
+    options = {};
+  }
+  options = options || {};
 
-	assert.ok(xmls instanceof Array, "argument 'xmls' must be an array");
-	assert.equal(typeof options, 'object', "argument 'options' must be an object");
+  assert.ok(xmls instanceof Array, "argument 'xmls' must be an array");
+  assert.equal(typeof options, 'object', "argument 'options' must be an object");
 
-	var zip = new JSZip();
-	var output = path.join(os.tmpdir(), uuid.v4() + '.pptx');
+  var zip = new JSZip();
+  var output = path.join(os.tmpdir(), uuid.v4() + '.pptx');
 
-	xmls.forEach(function (content) {
-		for (var key in content) {
-			if (content.hasOwnProperty(key)) {
-				var ext = key.substr(key.lastIndexOf('.'));
-				if (ext === '.xml' || ext === '.rels') {
-					var builder = new xml2js.Builder({
-						renderOpts: {
-							pretty: false
-						}
-					});
-					var xml2 = (builder.buildObject(content[key]));
-					zip.file(key, xml2);
-				} else {
-					zip.file(key, content[key]);
-				}
-			}
-		}
-	});
+  xmls.forEach(function (content) {
+    for (var key in content) {
+      if (content.hasOwnProperty(key)) {
+        var ext = key.substr(key.lastIndexOf('.'));
+        if (ext === '.xml' || ext === '.rels') {
+          var builder = new xml2js.Builder({
+            renderOpts: {
+              pretty: false
+            }
+          });
+          var xml2 = (builder.buildObject(content[key]));
+          zip.file(key, xml2);
+        } else {
+          zip.file(key, content[key]);
+        }
+      }
+    }
+  });
 
-	zip.generateAsync({
-		type: 'nodebuffer'
-	}).then(function (content) {
-		if (options.file) {
-			return fs.writeFile(output, content, function (err) {
-				if (err) {
-					return callback(err);
-				}
-				callback(null, output);
-			});
-		}
-		callback(null, content);
-	});
+  zip.generateAsync({
+    type: 'nodebuffer'
+  }).then(function (content) {
+    if (options.file) {
+      return fs.writeFile(output, content, function (err) {
+        if (err) {
+          return callback(err);
+        }
+        callback(null, output);
+      });
+    }
+    callback(null, content);
+  });
 };
 
 /**
@@ -152,11 +152,11 @@ Parser.prototype.bufferize = function bufferize(xmls, options, callback) {
  * @returns {string}
  */
 function uuid() {
-	var primary = (Math.random() * 46656) | 0;
-	var secondary = (Math.random() * 46656) | 0;
-	primary = ('000' + primary.toString(36)).slice(-3);
-	secondary = ('000' + secondary.toString(36)).slice(-3);
-	return primary + secondary;
+  var primary = (Math.random() * 46656) | 0;
+  var secondary = (Math.random() * 46656) | 0;
+  primary = ('000' + primary.toString(36)).slice(-3);
+  secondary = ('000' + secondary.toString(36)).slice(-3);
+  return primary + secondary;
 }
 
 module.exports = Parser;
