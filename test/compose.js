826561dd6a3b86d11002c61d0415a02585232b8f
shobhitsharma <ishobhitsharma@gmail.com>
2017-08-19 13:06:25 +0200

    Added tests

---
+++ test/compose.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var assert = require('assert');
+var Composer = require('../index.js');
+
+describe('Composer features', function () {
+  var compose = new Composer();
+  var sample_pptx = __dirname + '/sample.pptx';
+
+  it('should .parse() a PPTX file', function (done) {
+    compose.parse(sample_pptx, done);
+  });
+
+  it('should .execute() multiple PPTX files', function (done) {
+    compose.execute([sample_pptx], done);
+  });
+});
